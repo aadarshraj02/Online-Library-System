2bb697bff6edee734fd2c17097b0ead9d03e1c5b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 23:44:44 +0530

    imported and added routes for BookDetails

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
 import BrowseBooks from "./pages/BrowseBooks";
+import BookDetails from "./pages/BookDetails";
 
 const App = (): JSX.Element => {
   return (
@@ -12,6 +13,8 @@ const App = (): JSX.Element => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/books" element={<BrowseBooks />} />
+          <Route path="/books/:category" element={<BrowseBooks />} />
+          <Route path="/books/:category/:id" element={<BookDetails />} />
         </Routes>
         <Footer />
       </BrowserRouter>
