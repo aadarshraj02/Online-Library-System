e6a0bca92a13ee47430b6b880c79feb136ebf12c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 19:28:18 +0530

    added function to get conditional message

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,17 @@
 const HomePage = (): JSX.Element => {
+  const greeting = (): string => {
+    const currHour = new Date().getHours();
+    if (currHour < 12) return "Good Morning,";
+    if (currHour < 16) return "Good Afternoon,";
+    return "Good Evening,";
+  };
+
   return (
     <div className="bg-[url('/Background-image.jpeg')] min-h-screen relative bg-cover bg-center">
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div>
         <h1 className="text-center text-zinc-100 z-50 relative p-2 text-xl sm:text-2xl md:text-3xl font-semibold">
-          Good AfterNoon, Welcome to Online Library!
+          {greeting()} Welcome to Online Library!
         </h1>
       </div>
     </div>
