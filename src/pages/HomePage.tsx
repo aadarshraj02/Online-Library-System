a41949c621c0c711e3746f3d291475e798fdfbfe
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 19:22:48 +0530

    added bg image added header and styled header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 const HomePage = (): JSX.Element => {
   return (
-    <div>
+    <div className="bg-[url('/Background-image.jpeg')] min-h-screen relative bg-cover bg-center">
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div>
-        <h1>Good AfterNoon, Welcome to Online Library!</h1>
+        <h1 className="text-center text-zinc-100 z-50 relative p-2 text-xl sm:text-2xl md:text-3xl font-semibold">
+          Good AfterNoon, Welcome to Online Library!
+        </h1>
       </div>
     </div>
   );
