8a50511b871a9e9d7c73108e960938c4f20ad8e8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 21:08:10 +0530

    added basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = (): JSX.Element => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-zinc-900 text-white fixed w-full bottom-0 p-4 flex items-center justify-between">
+      <p>Online Library &#169; &#8482;</p>
+    </div>
+  );
 };
 
 export default Footer;
