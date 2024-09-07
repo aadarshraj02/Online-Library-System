8790712f77a0dcf3a26463a5cac60eb8b598ff74
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 20:26:47 +0530

    styled bookCard component

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,5 +1,21 @@
 const BookCard = (): JSX.Element => {
-  return <div>BookCard</div>;
+  return (
+    <div className="p-2">
+      <div className="relative bg-white rounded-lg h-[300px] w-[250px] shadow-lg bg-opacity-50 backdrop-blur-sm overflow-hidden">
+        <img
+          src="https://www.senseebooks.com/media/catalog/product/cache/fa9707e46bce5b7963f0dec14ddfaea9/n/o/nodavid_1.png"
+          alt=""
+          className="h-3/4 object-cover w-full"
+        />
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="text-xl text-center mb-2">Book Title</h1>
+          <button className="bg-green-600 px-4 py-1 rounded-md text-white hover:bg-green-700 transition-all ease-linear duration-200">
+            View Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookCard;
