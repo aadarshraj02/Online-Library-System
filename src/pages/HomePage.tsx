46677bcad1fbfadcd74f87b1c2afd8b5158b5ba8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 19:47:05 +0530

    fixed the ui

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,10 +9,12 @@ const HomePage = (): JSX.Element => {
   return (
     <div className="bg-[url('/Background-image.jpeg')] min-h-screen relative bg-cover bg-center">
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <div>
-        <h1 className="text-center text-zinc-100 z-50 relative p-2 text-xl sm:text-2xl md:text-3xl font-semibold">
-          {greeting()} Welcome to Online Library!
-        </h1>
+      <div className="z-50 relative p-2">
+        <div>
+          <h1 className="text-center text-zinc-100  text-xl sm:text-2xl md:text-3xl font-semibold">
+            {greeting()} Welcome to Online Library!
+          </h1>
+        </div>
       </div>
     </div>
   );
