79c8006c8bdbade67f5e93e7dc02e240d9b6b146
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-09 20:09:41 +0530

    get and displayed book title according to id

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,7 +1,17 @@
+import { useParams } from "react-router-dom";
+import { books } from "../utils/BookData";
+
 const BookDetails = (): JSX.Element => {
+  const { id } = useParams<{ id: any }>();
+  const book = books.find((b) => b.id == id);
   return (
-    <div className="bg-[url('/bookDetails.jpeg')] min-h-[82vh] bg-cover">
-      BookDetails
+    <div className="bg-[url('/bookDetails.jpeg')] min-h-[82vh] bg-cover relative">
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="relative z-10 bg-white bg-opacity-50 backdrop-blur-sm p-4 rounded-lg w-full max-w-[330px] sm:max-w-md">
+        <div>
+          <h1>{book?.title}</h1>
+        </div>
+      </div>
     </div>
   );
 };
