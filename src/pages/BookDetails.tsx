32d325564562aeb0b649623f5fcbecc7ae03d6d2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-09 19:45:31 +0530

    added bg image

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,5 +1,9 @@
 const BookDetails = (): JSX.Element => {
-  return <div>BookDetails</div>;
+  return (
+    <div className="bg-[url('/bookDetails.jpeg')] min-h-[82vh] bg-cover">
+      BookDetails
+    </div>
+  );
 };
 
 export default BookDetails;
