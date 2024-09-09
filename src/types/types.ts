779086ad03e1ca3d8091860bb61c859032636689
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-09 23:17:40 +0530

    added typeScript interface for books

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface Book {
+    id: number;
+    title: string;
+    image: string;
+    description: string;
+    author: string;
+    releaseYear: string;
+    rating: number;
+    pages: number;
+    category: string;
+  }
+  
