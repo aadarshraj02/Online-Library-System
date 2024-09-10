21727629091c2a542bd6c0daa2fc96ccb8564724
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-10 12:51:12 +0530

    added error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = (): JSX.Element => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="min-h-[82vh] flex flex-col items-center justify-center bg-zinc-100">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-lg mb-6">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Link
+        to="/"
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-all duration-300 ease-linear"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
