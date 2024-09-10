082afc683be549f4755606a2b74468a38da9da34
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-10 12:09:22 +0530

    added unique id for newly added book

--- src/redux/slices/booksSlice.ts
+++ src/redux/slices/booksSlice.ts
@@ -4,18 +4,22 @@ import { books as initialBooks } from '../../utils/BookData';
 
 interface BooksState {
   books: Book[];
+  nextId : number;
 }
 
 const initialState: BooksState = {
   books: JSON.parse(sessionStorage.getItem('books') || '[]') && initialBooks,
+  nextId: Math.max(0, ...initialBooks.map(book => book.id)) + 1,
 };
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-    addBook: (state, action: PayloadAction<Book>) => {
-      const updatedBooks = [...state.books, action.payload];
+    addBook: (state, action: PayloadAction<Omit<Book, 'id'>>) => {
+      const newBook = { ...action.payload, id: state.nextId };
+      state.nextId += 1;
+      const updatedBooks = [...state.books, newBook];
       state.books = updatedBooks;
       sessionStorage.setItem('books', JSON.stringify(updatedBooks));
     },
