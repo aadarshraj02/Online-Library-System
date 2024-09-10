1335410280a1d2433612ed405fa5f6d9f0fc3b73
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-10 09:44:05 +0530

    added yup validation

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -1,3 +1,26 @@
+import * as Yup from "yup";
+
+const validation = Yup.object({
+  title: Yup.string().required("Title is required"),
+  author: Yup.string().required("Author is required"),
+  image: Yup.string().url("Invalid URL").required("Image URL is required"),
+  description: Yup.string().required("Description is required"),
+  releaseYear: Yup.string().required("Release Year is required"),
+  rating: Yup.number().min(0).max(5).required("Rating is required"),
+  pages: Yup.number().required("Number of Pages is required"),
+  category: Yup.string()
+    .oneOf([
+      "Fiction",
+      "Non-Fiction",
+      "Sci-Fi",
+      "Romance",
+      "Mystery",
+      "Self-help",
+      "True-Crime",
+    ])
+    .required("Category is required"),
+});
+
 const AddBook = (): JSX.Element => {
   return (
     <div className="bg-[url('/bgimg.jpg')] min-h-[82vh] bg-cover bg-center relative w-full items-center flex justify-center">
