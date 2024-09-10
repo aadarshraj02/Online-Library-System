769e93c83d19ea9eb0ad414137f134d8aac656b3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-10 12:46:49 +0530

    imported and added routes for error page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import BrowseBooks from "./pages/BrowseBooks";
 import BookDetails from "./pages/BookDetails";
 import AddBook from "./pages/AddBook";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = (): JSX.Element => {
   return (
@@ -17,6 +18,7 @@ const App = (): JSX.Element => {
           <Route path="/books/:category" element={<BrowseBooks />} />
           <Route path="/book/:id" element={<BookDetails />} />
           <Route path="/add-books" element={<AddBook />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
